44bf0bd52f613f6f5bccc013c5c1f7b6d0c23856
User-on-Mars <weweebo@gmail.com>
2025-12-27 19:05:51 +0545

    Update useAuth hook to integrate with backend authentication

--- frontend/src/useAuth.js
+++ frontend/src/useAuth.js
@@ -3,8 +3,6 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase.js";
 
-
-
 // listens for Firebase auth state changes
 export function useAuth() {
   const [user, setUser] = useState(undefined); // undefined = loading
